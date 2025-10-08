9d2382e5b8f0e67f870b0e5a1777f896841e2a4e
jrat0007 <jrat0007@student.monash.edu>
2025-10-08 21:15:41 +1100

    Delete co2_emissions.js

--- co2_emissions.js
+++
@@ -1,4 +0,0 @@
-var vg_2 = "co2_emissions.vg.json";
-vegaEmbed("#line_chart", vg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
