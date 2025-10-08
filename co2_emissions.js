d07cb2bc798c8a7c7c20715e92a82fb7bde87f1c
jrat0007 <jrat0007@student.monash.edu>
2025-10-08 21:12:36 +1100

    Add files via upload

---
+++ co2_emissions.js
@@ -0,0 +1,4 @@
+var vg_2 = "co2_emissions.vg.json";
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
