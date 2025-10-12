0d8139eb05b81d067889e966a18de602e1917ef5
jrat0007 <jrat0007@student.monash.edu>
2025-10-12 23:35:30 +1100

    Add files via upload

--- vega_lite_plots.js
+++ vega_lite_plots.js
@@ -1,16 +1,29 @@
 var vg_1 = "airport_traffic.vg.json";
-vegaEmbed("#proportional_symbol_map", vg_1).then(function(result) {
+vegaEmbed("#proportional_symbol_map", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "co2_emissions_1.vg.json";
-vegaEmbed("#line_chart_1", vg_2).then(function(result) {
+vegaEmbed("#line_chart_1", vg_2).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "co2_emissions_2.vg.json";
-vegaEmbed("#line_chart_2", vg_3).then(function(result) {
+vegaEmbed("#line_chart_2", vg_3).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_4 = "australian_aviation_energy_usage.vg.json";
+vegaEmbed("#line_chart_3", vg_4).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
+var vg_5 = "australian_passenger_aircraft_movements.vg.json";
+vegaEmbed("#pie_chart_line_chart", vg_5).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "arrivals_departures.vg.json";
+vegaEmbed("#multi_line_chart", vg_6).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
