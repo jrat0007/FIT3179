1e99dc64cb127d60452ebcd9b1a8013e165cf0f2
jrat0007 <jrat0007@student.monash.edu>
2025-10-12 15:38:40 +1100

    Update vega_lite_plots.js

--- vega_lite_plots.js
+++ vega_lite_plots.js
@@ -4,7 +4,13 @@ vegaEmbed("#proportional_symbol_map", vg_1).then(function(result) {
 }).catch(console.error);
 
 var vg_2 = "co2_emissions_1.vg.json";
-vegaEmbed("#line_chart", vg_2).then(function(result) {
+vegaEmbed("#line_chart_1", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "co2_emissions_2.vg.json";
+vegaEmbed("#line_chart_2", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
 
