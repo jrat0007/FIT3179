e9e18aceed675917be4930602e943e5cf25cd200
jrat0007 <jrat0007@student.monash.edu>
2025-10-12 23:57:22 +1100

    Update vega_lite_plots.js

--- vega_lite_plots.js
+++ vega_lite_plots.js
@@ -1,29 +1,29 @@
-var vg_1 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/airport_traffic.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/Visualisations/airport_traffic.vg.json";
 vegaEmbed("#proportional_symbol_map", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/co2_emissions_1.vg.json";
+var vg_2 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/Visualisations/co2_emissions_1.vg.json";
 vegaEmbed("#line_chart_1", vg_2).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/co2_emissions_2.vg.json";
+var vg_3 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/Visualisations/co2_emissions_2.vg.json";
 vegaEmbed("#line_chart_2", vg_3).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_4 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/australian_aviation_energy_usage.vg.json";
+var vg_4 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/Visualisations/australian_aviation_energy_usage.vg.json";
 vegaEmbed("#line_chart_3", vg_4).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_5 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/australian_passenger_aircraft_movements.vg.json";
+var vg_5 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/Visualisations/australian_passenger_aircraft_movements.vg.json";
 vegaEmbed("#pie_chart_line_chart", vg_5).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_6 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/arrivals_departures.vg.json";
+var vg_6 = "https://raw.githubusercontent.com/jrat0007/FIT3179/refs/heads/main/Visualisations/arrivals_departures.vg.json";
 vegaEmbed("#multi_line_chart", vg_6).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
